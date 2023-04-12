23077e305ac7e18fd94355b5d90c9ec29fc951de
PaoloCarugati <paolocarugati76@gmail.com>
2023-04-13 01:13:05 +0200

    commit uno

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ArtistComponent } from './artist/artist.component';
+import { SearchComponent } from './search/search.component';
+import { TrackComponent } from './track/track.component';
+import { AlbumComponent } from './album/album.component';
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'tracks/:id', component: TrackComponent },
+  { path: 'artist/:id', component: ArtistComponent },
+  { path: 'albums/:id', component: AlbumComponent },
+  { path: '',   redirectTo: '/search', pathMatch: 'full' }
+]
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
